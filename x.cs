7195bcb563c92626c171fe2ec1a120eaceb7156b
oskari6 <oskarisulkakoski@gmail.com>
2024-03-20 06:30:27 +0200

    3202024

---
+++ x.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class x
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
