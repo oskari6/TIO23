e8c583b3ae41cdf179ed1927e40c2a5ce6ba8912
oskari6 <oskarisulkakoski@gmail.com>
2024-03-25 13:26:46 +0200

    25032024

---
+++ OOP/namespace/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Mail;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Reflection.Metadata.BlobBuilder;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+class Esimerkki6_8
+{
+    static void Main(string[] args)
+    {
+        //Tässä luodaan asiakas-olio, joka on instanssi  
+        //Sovellus.Kayttoliittyma.Asiakas-luokasta. Huomaa, 
+        //että luokan nimi joudutaan merkitsemään 
+        //nimiavaruuksien kautta. 
+        Sovellus.Kayttoliittyma.Asiakas asiakas = new
+        Sovellus.Kayttoliittyma.Asiakas("Sara");
+
+        //Tässä kutsutaan TarkistaAsiakas()-metodi.
+        asiakas.TarkistaAsiakas("Susan");
+
+        //Tässä kutsutaan taas TarkistaAsiakas()-metodi uudella 
+        //parametrin arvolla.
+        asiakas.TarkistaAsiakas("Sara");
+
+        //Tässä luodaan yhteys-olio, joka on instanssi 
+        //Sovellus.Tietokantayhteys.AvaaYhteys-luokasta.
+        Sovellus.Tietokantayhteys.AvaaYhteys yhteys = new
+        Sovellus.Tietokantayhteys.AvaaYhteys();
+        Console.WriteLine("Loppu");
+        Console.ReadLine();
+    }
+}
+
---
+++ OOP/namespace/Sovellus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Seuraavassa määritellään Sovellus-nimiavaruus.
+namespace Sovellus
+{
+    //Seuraavassa määritellään Sovellus.Kayttoliittyma-
+    //nimiavaruus.
+    namespace Kayttoliittyma
+    {
+        //Seuraavassa määritellään 
+        //Sovellus.Kayttoliittyma.Asiakas-luokka.
+        class Asiakas
+        {
+            string nimi;
+
+            public Asiakas(string nimi)
+            {
+                this.nimi = nimi;
+            }
+
+            //Seuraavassa määritellään TarkistaAsiakas()-metodi, 
+            //jolla asiakkaan nimeä verrataan parametrina olevaan 
+            //nimeen ja sen perusteella ilmoitetaan onko asiakkaan 
+            //nimi oikein vai ei.
+            public void TarkistaAsiakas(string nimi)
+            {
+                //Tässä verrataan nimi-kentän ja nimi-parametrin 
+                //arvoja sisäänrakennetulla Equals()-metodilla.
+                if (this.nimi.Equals(nimi))
+                    System.Console.WriteLine(nimi + " on asiakkaan oikea nimi.");
+                else
+                    System.Console.WriteLine(nimi + " ei ole asiakkaan oikea nimi!");
+            }
+        } //Sovellus.Kayttoliittyma.Asiakas-luokka loppuu tähän.
+    } //Sovellus.Kayttoliittyma-nimiavaruusloppuu tähän.
+} //Sovellus-nimiavaruus loppuu tähän.
